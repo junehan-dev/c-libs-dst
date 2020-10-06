19ea172f47be3ae57887ef0144ffaededa186688
junehan <junehan.dev@gmail.com>
2020-10-06 11:04:11 +0900

    lstnew, lstsize

--- srcs/ft_lstsize.c
+++ srcs/ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: jihhan <junehan.dev@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/24 15:34:33 by jihhan            #+#    #+#             */
-/*   Updated: 2020/09/24 15:36:51 by jihhan           ###   ########.fr       */
+/*   Updated: 2020/10/06 11:03:06 by jihhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int                 ft_lstsize(t_list *lst)
     int     len;
     t_list  *lst_pt;
  
-    if (!lst)
+    if (!lst || (!(lst->content) && !(lst->next)))
         return (0);
 
     len = 0;
