0314f3f1a1277e4782bbfa6f70ca392cf8a7e378
junehan <junehan.dev@gmail.com>
2020-11-10 14:59:41 +0900

    refactor/lst_add_front

--- srcs/ft_lstadd_front.c
+++ srcs/ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: jihhan <junehan.dev@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/24 15:33:38 by jihhan            #+#    #+#             */
-/*   Updated: 2020/10/21 16:38:37 by jihhan           ###   ########.fr       */
+/*   Updated: 2020/11/10 14:58:54 by jihhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,11 @@
 
 void                ft_lstadd_front(t_list **lst, t_list *new)
 {
-    t_list **lst_pt;
-    t_list *temp;
-    t_list *next;
+    t_list  *temp;
 
     if (!lst || !new)
         return;
 
-    lst_pt = lst;
-    next = new;
-
-    while (next && *lst_pt)
-    {
-        temp = *lst_pt;
-        *lst_pt = next;
-        next = temp;
-        lst_pt++;
-    }
-
-    if (*(lst_pt + 1) != NULL)
-    {
-        free(next);
-        return;
-    }    
-
-    *lst_pt = next;
+    new->next = *lst;
+    *lst = new;
 }
